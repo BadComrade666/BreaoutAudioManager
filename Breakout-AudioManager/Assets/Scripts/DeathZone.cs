254ed2c621b4e7a444e6124c1fc7328a4654e875
Jean-Luc Sinclair <jcohen11@berklee.edu>
2019-02-25 12:18:29 -0500

    Simple Audio Manger

---
+++ Breakout-AudioManager/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+		GM.instance.LoseLife();
+		SoundManager.instance.LifeLoss ();
+	}
+}
---
+++ Breakout-AudioManager/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Singleton
+
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip brick, wall, paddle, youWin, youLose, lifeLoss;
+	public static int impactType = 0;
+	public static SoundManager instance = null;
+
+	public AudioSource soundSource;
+    public AudioSource soundSource02;
+
+
+    // Use this for initialization
+    void Start () 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+	}
+		
+		public void ImpactSound()
+	{
+		if (impactType == 1)
+
+			soundSource.clip = brick;
+
+		if (impactType == 2)
+
+			soundSource.clip = wall;
+
+		if (impactType == 3)
+
+			soundSource.clip = paddle;
+
+		soundSource.Play ();
+	}
+
+
+	public void PaddleSound()
+
+	{
+		soundSource02.clip = paddle;
+		soundSource02.Play ();
+	}
+
+	public void YouWin()
+
+	{
+		soundSource02.clip = youWin;
+		soundSource02.Play ();
+	}
+
+	public void YouLose()
+
+	{
+		soundSource02.clip = youLose;
+		soundSource02.Play ();
+	}
+
+	public void LifeLoss()
+
+	{
+		soundSource02.clip = lifeLoss;
+		soundSource02.Play ();
+	}
+
+}
